c815418ffe36a2cb6b8d433635ae4520d5cb3c14
yamamura ryoga <yamamuraryoga@MacBook-Pro-6.local>
2022-07-09 17:59:56 +0900

    add project

---
+++ SwiftConcurrencySample/SwiftConcurrencySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencySampleApp.swift
+//  SwiftConcurrencySample
+//
+//  Created by yamamura ryoga on 2022/07/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
