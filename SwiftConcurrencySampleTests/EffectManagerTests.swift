bac70083997e2ebf7a990fa1ccf5538e1d2b0776
yamamura ryoga <ryoga1226@gmail.com>
2022-09-10 17:11:41 +0900

    test修正

--- SwiftConcurrencySampleTests/EffectManagerTests.swift
+++ SwiftConcurrencySampleTests/EffectManagerTests.swift
@@ -23,146 +23,157 @@ final class EffectManagerTests: XCTestCase {
         let expectation = XCTestExpectation(description: "add")
         expectation.expectedFulfillmentCount = 1
 
-        let task = Task<Void, Never>.detached {
-            try! await Task.sleep(nanoseconds: 1_000_000_000)
+        // 追加されるか
+        effectManager.add(DefaultEffectID()) {
+            try? await Task.sleep(nanoseconds: 500_000_000)
             expectation.fulfill()
         }
-        // 追加されるか
-        effectManager.add(DefaultEffectID(), task: task)
         XCTAssertEqual(effectManager.effects[DefaultEffectID()]!.count, 1)
 
-        // taskが成功したら削除されてるか
+        // taskの完了を待って削除されてるか
+        let task = effectManager.effects[DefaultEffectID()]!.first!
         await task.value
         wait(for: [expectation], timeout: 3.0)
         XCTAssertEqual(effectManager.effects[DefaultEffectID()]!.count, 0)
     }
     
     func testCancell() async throws {
-        let task = Task<Void, Never>.detached {
+        effectManager.add(DefaultEffectID()) {
             try? await Task.sleep(nanoseconds: 2_000_000_000)
         }
-        
-        effectManager.add(DefaultEffectID(), task: task)
+
+        let task = effectManager.effects[DefaultEffectID()]!.first!
         effectManager.cancell(DefaultEffectID())
-        
+
         XCTAssertTrue(task.isCancelled)
     }
-        
+
     func testAllCancell() async throws {
-        let task1 = Task<Void, Never>.detached {
+        effectManager.add(DefaultEffectID()) {
             try? await Task.sleep(nanoseconds: 2_000_000_000)
         }
-        effectManager.add(DefaultEffectID(), task: task1)
+        let task1 = effectManager.effects[DefaultEffectID()]!.first!
 
-        let task2 = Task<Void, Never>.detached {
+
+        effectManager.add(DefaultEffectID()) {
             try? await Task.sleep(nanoseconds: 2_000_000_000)
         }
-        effectManager.add(DefaultEffectID(), task: task2)
+        let task2 = effectManager.effects[DefaultEffectID()]![1]
 
-
-        let task3 = Task<Void, Never>.detached {
+        effectManager.add(DefaultEffectID()) {
             try? await Task.sleep(nanoseconds: 2_000_000_000)
         }
-        effectManager.add(DefaultEffectID(), task: task3)
+        let task3 = effectManager.effects[DefaultEffectID()]![2]
 
         effectManager.cancell(DefaultEffectID())
         XCTAssertTrue(task1.isCancelled)
         XCTAssertTrue(task2.isCancelled)
         XCTAssertTrue(task3.isCancelled)
     }
-    
+
+    // cancel回数が正しいか
     func testCanncellAndAddForCancellCase() async throws {
         let expectation = XCTestExpectation(description: "cancellAndAdd")
         expectation.expectedFulfillmentCount = 2
 
-        let task1 = Task<Void, Never>.detached {[weak self] in
+        effectManager.cancellAndAdd(DefaultEffectID()) {
             try? await Task.sleep(nanoseconds: 1_000_000_000)
             if Task.isCancelled {
-                self!.exceptValue += 1
+                self.exceptValue += 1
             }
             expectation.fulfill()
         }
-        effectManager.cancellAndAdd(DefaultEffectID(), task: task1)
-        
-        let task2 = Task<Void, Never>.detached {[weak self] in
+        let task1 = effectManager.effects[DefaultEffectID()]!.first!
+
+        effectManager.cancellAndAdd(DefaultEffectID()) {
             try? await Task.sleep(nanoseconds: 1_000_000_000)
             if Task.isCancelled {
-                self!.exceptValue += 1
+                self.exceptValue += 1
             }
             expectation.fulfill()
         }
-        effectManager.cancellAndAdd(DefaultEffectID(), task: task2)
-        
-        wait(for: [expectation], timeout: 10.0)
+        // この時点で2回目のcanncellAndAddが呼ばれてるので、taskの配列からtask1は削除されてるためindexは0
+        let task2 = effectManager.effects[DefaultEffectID()]!.first!
+        _ = (await task1.value, await task2.value)
+
+        wait(for: [expectation], timeout: 5.0)
         XCTAssertEqual(exceptValue, 1)
     }
-    
+
+    // cancel回数(exceptValue)が正しいか
     func testCanncellAndAddForNotCancellCase() async throws {
         let expectation = XCTestExpectation(description: "cancellAndAdd")
         expectation.expectedFulfillmentCount = 2
 
-        let task1 = Task<Void, Never>.detached {[weak self] in
+        effectManager.cancellAndAdd(DefaultEffectID()) {
             try? await Task.sleep(nanoseconds: 1_000_000_000)
-            if Task.isCancelled == false {
-                self!.exceptValue += 1
+            if Task.isCancelled {
+                self.exceptValue += 1
             }
             expectation.fulfill()
         }
-        effectManager.cancellAndAdd(DefaultEffectID(), task: task1)
+        let task1 = effectManager.effects[DefaultEffectID()]!.first!
         
+        // task1の完了を待つ
         try? await Task.sleep(nanoseconds: 1_500_000_000)
-        
-        let task2 = Task<Void, Never>.detached {[weak self] in
+
+        effectManager.cancellAndAdd(DefaultEffectID()) {
             try? await Task.sleep(nanoseconds: 1_000_000_000)
-            if Task.isCancelled == false {
-                self!.exceptValue += 1
+            if Task.isCancelled {
+                self.exceptValue += 1
             }
             expectation.fulfill()
         }
-        effectManager.cancellAndAdd(DefaultEffectID(), task: task2)
-        
+        // この時点でtask1は完了してるので、taskの配列からtask1は削除されてるためindexは0
+        let task2 = effectManager.effects[DefaultEffectID()]![0]
+
+        // task1の完了を待ってからcancellAndAdd呼び出すので、exceptValueは0に
+        _ = (await task1.value, await task2.value)
         wait(for: [expectation], timeout: 10.0)
-        XCTAssertEqual(exceptValue, 2)
+        XCTAssertEqual(exceptValue, 0)
     }
 
-    
+
     func testCanncellAndAddThreeTime() async throws {
         let expectation = XCTestExpectation(description: "cancellAndAdd")
         expectation.expectedFulfillmentCount = 3
 
-        let task1 = Task<Void, Never>.detached {[weak self] in
+        effectManager.cancellAndAdd(DefaultEffectID()) {
             try? await Task.sleep(nanoseconds: 1_000_000_000)
-            if Task.isCancelled == false {
-                self!.exceptValue += 1
+            if Task.isCancelled {
+                self.exceptValue += 1
             }
             expectation.fulfill()
         }
-        effectManager.cancellAndAdd(DefaultEffectID(), task: task1)
-        
-        try? await Task.sleep(nanoseconds: 1_500_000_000)
-        
-        let task2 = Task<Void, Never>.detached {[weak self] in
-            try? await Task.sleep(nanoseconds: 1_000_000_000)
-            if Task.isCancelled == false {
-                self!.exceptValue += 1
-            }
-            expectation.fulfill()
-        }
-        effectManager.cancellAndAdd(DefaultEffectID(), task: task2)
-        
-//        try? await Task.sleep(nanoseconds: 1_500_000_000)
-        
-        let task3 = Task<Void, Never>.detached {[weak self] in
-            try? await Task.sleep(nanoseconds: 1_000_000_000)
-            if Task.isCancelled == false {
-                self!.exceptValue += 1
-            }
-            expectation.fulfill()
-        }
-        effectManager.cancellAndAdd(DefaultEffectID(), task: task3)
+        let task1 = effectManager.effects[DefaultEffectID()]!.first!
 
-        
+        // task1の完了を待つ
+        try? await Task.sleep(nanoseconds: 1_500_000_000)
+
+        effectManager.cancellAndAdd(DefaultEffectID()) {
+            try? await Task.sleep(nanoseconds: 1_000_000_000)
+            if Task.isCancelled {
+                self.exceptValue += 1
+            }
+            expectation.fulfill()
+        }
+        let task2 = effectManager.effects[DefaultEffectID()]!.first!
+
+        // task2の完了を待つ場合
+//        try? await Task.sleep(nanoseconds: 1_500_000_000)
+
+        effectManager.cancellAndAdd(DefaultEffectID()) {
+            try? await Task.sleep(nanoseconds: 1_000_000_000)
+            if Task.isCancelled {
+                self.exceptValue += 1
+            }
+            expectation.fulfill()
+        }
+        let task3 = effectManager.effects[DefaultEffectID()]!.first!
+
+        _ = (await task1.value, await task2.value, await task3.value)
         wait(for: [expectation], timeout: 10.0)
-        XCTAssertEqual(exceptValue, 2)
+        // task2のみキャンセルするので
+        XCTAssertEqual(exceptValue, 1)
     }
 }
